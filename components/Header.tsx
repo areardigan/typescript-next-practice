3e6149dca63b1d3fdd51d76fadf77ac3b0f5f039
andrewreardigan <drewreardigan@gmail.com>
2022-04-20 23:58:48 -0400

    header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex">
+    <header className="mx-auto flex  max-w-7xl justify-between p-5">
+      <div className="flex items-center space-x-5">
         <Link href="/">
           <img
             className="w-44 cursor-pointer object-contain"
@@ -15,8 +15,14 @@ const Header = () => {
         <div className="hidden items-center space-x-5 md:inline-flex ">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
         </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign in</h3>
+        <h3 className="rounded-full border px-4 py-1">Get started</h3>
       </div>
     </header>
   )
